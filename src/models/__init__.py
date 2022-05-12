fb3d9ae32ed13a8ebf6f62d61db4b93e21601ab8
Amirhossein Kazemnejad <2122102+kazemnejad@users.noreply.github.com>
2022-05-12 17:47:02 -0400

    Add support for gpt2

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,7 @@
 from .base_model import Model, HfModelConfig
 from .t5 import Seq2SeqT5
 from .gpt_neo import CausalGPTNeo, SeqClassifierGPTNeo
+from .gpt2 import CausalGPT2, SeqClassifierGPT2
 from .custom_t5_decoder_only import CustomDecoderOnlyT5
 
 
